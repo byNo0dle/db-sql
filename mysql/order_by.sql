138ed2c76e80bd53fa33c2a87c489aa7b94e1d3c
Nick Saurino <nicksaurinocruz@gmail.com>
2024-08-06 19:42:48 -0500

    Using order_by in sql

---
+++ mysql/order_by.sql
@@ -0,0 +1,9 @@
+SELECT * FROM stocks ORDER BY quantity;
+
+SELECT * FROM stocks ORDER BY quantity ASC;
+
+SELECT * FROM stocks ORDER BY quantity DESC;
+
+SELECT * FROM stocks WHERE code = 50 ORDER BY quantity DESC;
+
+SELECT id FROM stocks WHERE code = 50 ORDER BY quantity DESC;
